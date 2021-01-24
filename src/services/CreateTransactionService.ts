179e655724f569d22e857c9f00293e09a304f993
JUliano Luiz da Silva <juliano.silva@wisereducacao.com>
2021-01-24 15:58:57 -0300

    feat: finish test

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,14 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface Request {
+  title: string;
+
+  value: number;
+
+  type: 'income' | 'outcome';
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +16,22 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({
+    title,
+    value,
+    type
+  }:Request): Transaction {
+
+    if(type === 'outcome'
+      && this.transactionsRepository.foundsAreAvailable(value) === false) {
+      throw Error("there are no funds for the transaction");
+    }
+
+    return this.transactionsRepository.create({
+      title,
+      value,
+      type
+    });
   }
 }
 
